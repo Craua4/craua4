e51b8beb3ab50de92963bcb3f0523150f447a5b0
Craua4 <1kaziinho@gmail.com>
2026-08-20 18:30:39 -0300

    NOVO 20/08

--- react/tcc/src/App.jsx
+++ react/tcc/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import graficodepiza from './assets/graficodepiza.png'
 import './App.css'
 
 function App() {
@@ -10,6 +11,29 @@ function App() {
   return(
     <div>
       <header>POPULATIONAL GROWTH</header>
+        <div class="right">
+          <b>
+            <h1>
+              What is populational growth?
+            </h1>
+          </b>
+          <p>
+            Population growth happens when birth rate is higher than death rate.
+            <br/>
+            It can be influenced by imigration, though, not relevant when we talk about the world.
+          </p>
+        </div>
+        <div class="left">
+          <b>
+            <h1>
+              Data over the years
+            </h1>
+          </b>
+          <img src="graficodepiza.png" alt="" />
+          <p>
+
+          </p>
+        </div>
 
 
       
